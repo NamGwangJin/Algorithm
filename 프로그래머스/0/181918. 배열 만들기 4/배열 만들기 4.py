a84051d26754c4aef8b7ed23eab2488e360c1466
남광진(NAM GWANG JIN) <137127472+NamGwangJin@users.noreply.github.com>
2024-01-27 15:58:46 +0900

    [level 0] Title: 배열 만들기 4, Time: 7.68 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i += 1
+            continue
+        else:
+            if stk[len(stk) - 1] < arr[i]:
+                stk.append(arr[i])
+                i += 1
+                continue
+            else:
+                stk.remove(stk[len(stk) - 1])
+    return stk
