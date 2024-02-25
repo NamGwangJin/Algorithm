fd633ffb9eb223d40d8675e4eefd4e8404bc66b7
남광진(NAM GWANG JIN) <137127472+NamGwangJin@users.noreply.github.com>
2024-02-25 11:41:50 +0900

    Update 문자 개수 세기.java

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -2,19 +2,15 @@ class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
         
-        for ( int i = 0; i < answer.length; i++) {
-        	answer[i] = 0;
-        }
-        
         for ( int i = 0; i < my_string.length(); i++) {
         	char alphabet = my_string.charAt(i);
         	
         	if (alphabet >= 'A' && alphabet <= 'Z') {
             	int order = (int) alphabet - 'A';
-            	answer[order] += 1;
+            	answer[order]++;
         	} else {
             	int order = (int) alphabet - 'A' - 6;
-            	answer[order] += 1;
+            	answer[order]++;
         	}
         }
         
