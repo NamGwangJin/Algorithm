6fc09023b9109cc37cb5279182551830bebcbddc
남광진(NAM GWANG JIN) <137127472+NamGwangJin@users.noreply.github.com>
2024-03-14 18:10:48 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        for ( int i = 0 ; i <= num_list.length ; i++ ) {
+            if ( i == num_list.length ) {
+                answer = -1;
+                break;
+            }
+            
+            if ( num_list[i] < 0 ) {
+                answer = i;
+                break;
+            } 
+        }
+        
+        return answer;
+    }
+}
