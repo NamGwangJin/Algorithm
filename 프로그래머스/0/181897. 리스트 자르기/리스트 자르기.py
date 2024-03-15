7f8dfdf2c656f0aa439ae18c9b9369c39d09d2db
남광진(NAM GWANG JIN) <137127472+NamGwangJin@users.noreply.github.com>
2024-03-15 21:12:07 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,16 @@
+def solution(n, slicer, num_list):
+    answer = []
+
+    if n == 1:
+        answer = num_list[0 : slicer[1] + 1]
+
+    if n == 2:
+        answer = num_list[slicer[0] : len(num_list)]
+
+    if n == 3:
+        answer = num_list[slicer[0] : slicer[1] + 1]
+
+    if n == 4:
+        answer = num_list[slicer[0] : slicer[1] + 1 : slicer[2]]
+
+    return answer
